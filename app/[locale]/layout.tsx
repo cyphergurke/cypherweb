b447e53a885da240eaef3f7a6d5b12f1b9adfb2c
cyphergurke <cyberzuchini@protonmail.com>
2024-08-23 13:55:12 +0200

    init nextjs app with shadcnui, intl i18, tailwindcss, typescript, themes

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Davids Portfolio",
+  description: "Modern & Minimal Web developer Portfolio",
+};
+
+export default async function RootLayout({
+  children,
+  params: { locale }
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: string };
+}>) {
+
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <body className={inter.className}>
+        <NextIntlClientProvider messages={messages}>
+
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html >
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+
+import createNextIntlPlugin from 'next-intl/plugin';
+
+/** @type {import('next').NextConfig} */
+const withNextIntl = createNextIntlPlugin();
+
+const nextConfig = {};
+
+
+export default withNextIntl(nextConfig);
