985844696f16715bc86d8b3b4126e1cfb5209c93
cyphergurke <cyberzuchini@protonmail.com>
2024-08-23 13:58:35 +0200

    add hero section with shadcnui/aceternity ui components

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,164 @@
+import { navItems, workExperience } from "@/data";
+
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { useTranslations } from 'next-intl';
+import About from "@/components/About";
+import { ContactForm } from "@/components/Contact";
+import MobileMenu from "@/components/MobileMenu";
+
+
+
+export default function Home() {
+
+  const h = useTranslations('Hero');
+  const heroTranslations = {
+    title: h('title'),
+    subTitle: h('subTitle'),
+    navHome: h('navigation.home'),
+    navAbout: h('navigation.about'),
+    navprojects: h('navigation.projects'),
+    navContact: h('navigation.contact'),
+    buttonText: h('buttonText'),
+    language: h('language'),
+  };
+
+
+  const a = useTranslations('About');
+  const aboutTranslations = {
+    title: a('title'),
+    text: a('text'),
+    communication: a('communication'),
+    timeZones: a('timeZones'),
+    techStack: a('techStack'),
+    passion: a('passion'),
+    specialist: a('specialist'),
+    startProject: a('startProject'),
+    requestFreelancerProfile: a('requestFreelancerProfile'),
+    copyEmail: a('copyEmail'),
+    emailCopied: a('emailCopied'),
+  }
+
+  const p = useTranslations('Projects');
+  const projectstranslations = {
+    title: p('title'),
+    BitcoinVanityTitle: p('BitcoinVanityTitle'),
+    BitcoinVanityDesc: p('BitcoinVanityDesc'),
+    OverStackingFlowTitle: p('OverStackingFlowTitle'),
+    OverStackingFlowDesc: p('OverStackingFlowDesc')
+  }
+
+  const ap = useTranslations('Approach');
+  const approachTranslation = {
+    title: ap('title'),
+    phase1: {
+      title: ap('phase1.title'),
+      text: ap('phase1.text'),
+    },
+    phase2: {
+      title: ap('phase2.title'),
+      text: ap('phase2.text'),
+    },
+    phase3: {
+      title: ap('phase3.title'),
+      text: ap('phase3.text'),
+    }
+  }
+
+
+
+  const navigationItems = navItems(heroTranslations)
+
+
+  const w = useTranslations('Experience');
+
+  const workTranslations = {
+    title: w('title'),
+    btcProfessional: {
+      title: w('btcProfessional.title'),
+      desc: w('btcProfessional.desc'),
+    },
+    intern: {
+      title: w('intern.title'),
+      desc: w('intern.desc'),
+    },
+    consultant: {
+      title: w('consultant.title'),
+      desc: w('consultant.desc'),
+    },
+    freelancer: {
+      title: w('freelancer.title'),
+      desc: w('freelancer.desc'),
+    }
+  }
+
+  const c = useTranslations('Contact');
+  const contactTranslation = {
+    title: c('title'),
+    subTitle: c('subTitle'),
+    name: {
+      label: c('name.label'),
+      placeholder: c('name.placeholder'),
+      errorMessage: c('name.errorMessage')
+    },
+    email: {
+      label: c('email.label'),
+      placeholder: c('email.placeholder'),
+      errorMessage: c('email.errorMessage')
+    },
+    subject: {
+      label: c('subject.label'),
+      placeholder: c('subject.placeholder'),
+    },
+    message: {
+      label: c('message.label'),
+      placeholder: c('message.placeholder'),
+      errorMessage: c('message.errorMessage')
+    },
+    agreeToTerms: {
+      label: c('agreeToTerms.label'),
+      errorMessage: c('agreeToTerms.errorMessage')
+    },
+    button: c('button'),
+    successMessage: c('successMessage'),
+    failedMessage: c('failedMessage'),
+    errorMessage: c('errorMessage')
+  }
+
+
+  const f = useTranslations("Footer")
+  const footerTranslation = {
+    getintouch: f('getintouch')
+  }
+
+
+  const workExperienceItems = workExperience(workTranslations)
+  const e = useTranslations("Experience")
+  const experienceTranslations = {
+    title: e('title'),
+  }
+
+  return (
+    <main className="relative dark:bg-black-100 bg-slate-300 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 ">
+      <div className="w-full z-50 sm:hidden block">
+        <MobileMenu navItems={navigationItems} translate={heroTranslations} />
+      </div>
+      <div className="max-w-7xl w-full px-5">
+        <div className="sm:block hidden">
+          <FloatingNav navItems={navigationItems} />
+        </div>
+        <Hero translate={heroTranslations} />
+        <About translate={aboutTranslations} />
+        <Experience translate={experienceTranslations} workExperienceItems={workExperienceItems} />
+        <Approach translate={approachTranslation} />
+        <RecentProjects translate={projectstranslations} />
+        <ContactForm translate={contactTranslation} />
+        <Footer translate={footerTranslation} />
+      </div>
+    </main >
+  );
+};
---
+++ components/Hero.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import ComputersCanvas from "./canvas/Computers";
+import Image from "next/image";
+const Hero = ({ translate }: any) => {
+  return (
+    <div className=" md:pt-36 lg:pt-36" id="home">
+      <div>
+        <Spotlight
+          className="h-[80vh] w-[77vw] top-44 left-full"
+          fill="purple"
+        />
+        <Spotlight
+          className="-top-40 -left-10 w-full md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[77vw]" fill="blue" />
+      </div>
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-slate-300 dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+        bg-slate-300 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+
+      <div className="flex flex-col lg:flex-row justify-center relative my-20  w-full   ">
+        <div className="w-full lg:w-2/3 flex flex-col items-center  justify-center">
+
+          <TextGenerateEffect
+            words={translate.title}
+            className="text-center lg:text-left text-[40px] md:text-5xl lg:text-6xl"
+          />
+          <TextGenerateEffect
+            words={translate.subTitle}
+            className="text-center lg:text-left md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl"
+          />
+        </div>
+        <div className=" h-[30vh] md:h-[40vh] lg:h-[50vh] w-full  -mt-50 ">
+          <ComputersCanvas />
+        </div>
+      </div>
+      <div className="  flex justify-center items-center   ">
+        <a href="#about" className=" lg:pt-20">
+          <Image src="DoubleArrowDown.svg" alt="About"
+            className="filter dark:invert animate-bounce " width={35} height={35} />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
