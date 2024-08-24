c9b915b9b675deec03ab88c429e737f2ef97e286
cyphergurke <cyberzuchini@protonmail.com>
2024-08-24 18:14:36 +0200

    fix dropdown body transition sideeffect bug

--- components/ui/LanguageToggle.tsx
+++ components/ui/LanguageToggle.tsx
@@ -1,44 +1,79 @@
 
-import React from 'react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './dropdown-menu'
-import Link from 'next/link'
-import { cn } from '@/lib/utils'
-import { GlobeIcon } from '@radix-ui/react-icons'
+import React, { useEffect, useRef, useState } from 'react'
+import { GlobeIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 const LanguageToggle = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const toggleDropdown = () => setIsOpen(!isOpen);
+
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+            setIsOpen(false);
+        }
+    };
+
+    const handleScroll = () => {
+        if (window.scrollY > 120) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+
     return (
-        <>
-            <DropdownMenu >
-                <DropdownMenuTrigger asChild>
-                    <button className="border flex text-sm font-medium relative border-neutral-200 transition-all duration-500 hover:border-violet-500   dark:border-white/[0.2] text-white  px-2 py-2 rounded-full">
-                        <GlobeIcon className='h-[1.2rem] w-[1.2rem] ' />
-                        <span className='sr-only'>Toggle theme</span>
-                    </button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className='pt-4' >
-                    <DropdownMenuItem >
-                        <Link
-                            href="/en"
-                            className={cn(
-                                "relative dark:text-white items-center  flex space-x-1 text-black   dark:hover:text-neutral-300 hover:text-neutral-500"
-                            )}
-                        >
-                            <span className=" text-sm !cursor-pointer">english</span>
-                        </Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem >
-                        <Link
-                            href="/de"
-                            className={cn(
-                                "relative dark:text-white items-center  flex space-x-1 text-black   dark:hover:text-neutral-300 hover:text-neutral-500"
-                            )}
-                        >
-                            <span className=" text-sm !cursor-pointer">deutsch</span>
-                        </Link>
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </>
+        <div className="relative inline-block text-left" ref={dropdownRef}>
+            <button
+                onClick={toggleDropdown}
+                className="border flex text-sm font-medium relative border-neutral-200 transition-all duration-500 hover:border-violet-500   dark:border-white/[0.2] text-white  px-2 py-2 rounded-full"
+            >
+                <GlobeIcon className='h-[1.2rem] w-[1.2rem] ' />
+                <span className="sr-only">Toggle language</span>
+            </button>
+            <div
+                className={cn(
+                    'origin-top-right absolute right-0 mt-2 p-2 rounded-md shadow-lg bg-white dark:bg-black-100 dark:shadow-white-200 dark:shadow-md transition-all ',
+                    isOpen
+                        ? 'opacity-100 max-h-60 max-w-xs duration-700 ease-in-out'
+                        : 'opacity-0 max-h-0 max-w-0 duration-700 ease-out overflow-hidden'
+                )}
+
+            >
+                <div className="py-1">
+                    <Link
+                        href="/en"
+                        className={cn(
+                            "relative dark:text-white items-center  flex space-x-1 text-black   dark:hover:text-neutral-300 hover:text-neutral-500 hover:bg-gray-100 rounded-md p-1"
+                        )}
+                        onClick={toggleDropdown}
+                    >
+                        <span className=" text-sm !cursor-pointer">english</span>
+                    </Link>
+                    <Link
+                        href="/de"
+                        className={cn(
+                            "relative dark:text-white items-center  flex space-x-1 text-black   dark:hover:text-neutral-300 hover:text-neutral-500 hover:bg-gray-100 rounded-md p-1"
+                        )}
+                        onClick={toggleDropdown}
+                    >
+                        <span className=" text-sm !cursor-pointer">deutsch</span>
+                    </Link>
+                </div>
+            </div>
+            {/*   )} */}
+        </div>
     )
 }
 
--- components/ui/ModeToggle.tsx
+++ components/ui/ModeToggle.tsx
@@ -1,40 +1,96 @@
 'use client';
 
-import * as React from 'react';
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { useTheme } from 'next-themes';
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+
+import React, { useEffect, useRef, useState } from 'react'
+import { cn } from '@/lib/utils';
+
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
 
 
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleScroll = () => {
+    if (window.scrollY > 120) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <DropdownMenu >
-      <DropdownMenuTrigger asChild>
-        <button className="border flex text-sm font-medium relative border-neutral-200 transition-all duration-500 hover:border-violet-500  dark:border-white/[0.2] text-white  px-2 py-2 rounded-full">
+    <>
+
+
+      <div className="relative inline-block text-left" ref={dropdownRef}>
+        <button
+          onClick={toggleDropdown}
+          className="border flex text-sm font-medium relative border-neutral-200 transition-all duration-500 hover:border-violet-500   dark:border-white/[0.2] text-white  px-2 py-2 rounded-full"
+        >
           <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0   ' />
           <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100  ' />
           <span className='sr-only'>Toggle theme</span>
         </button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className='pt-4' >
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        <div
+          className={cn(
+            'origin-top-right absolute mt-2 p-2 rounded-md shadow-lg bg-white dark:bg-black-100 dark:shadow-white-200 dark:shadow-md transition-all ',
+            isOpen
+              ? 'opacity-100 max-h-60 max-w-xs duration-700 ease-in-out'
+              : 'opacity-0 max-h-0 max-w-0 duration-700 ease-out overflow-hidden'
+          )}
+        >
+          <div className="py-1">
+            <button
+              className={cn(
+                "relative dark:text-white items-center  flex space-x-1 text-black   dark:hover:text-neutral-300 hover:text-neutral-500 hover:bg-gray-100 rounded-md p-1"
+              )}
+              onClick={() => { setTheme('light'); toggleDropdown }}
+            >
+              Light
+            </button>
+            <button
+              className={cn(
+                "relative dark:text-white items-center  flex space-x-1 text-black   dark:hover:text-neutral-300 hover:text-neutral-500 hover:bg-gray-100 rounded-md p-1"
+              )}
+              onClick={() => { setTheme('dark'); toggleDropdown }}
+            >
+              Dark
+            </button>
+            <button
+              className={cn(
+                "relative dark:text-white items-center  flex space-x-1 text-black   dark:hover:text-neutral-300 hover:text-neutral-500 hover:bg-gray-100 rounded-md p-1"
+              )}
+              onClick={() => { setTheme('system'); toggleDropdown }}
+            >
+              System
+            </button>
+
+          </div>
+        </div>
+      </div>
+
+    </>
   );
 }
