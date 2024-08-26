fd6832fc9ea76a68352acfb5dda6967748d722c0
cyphergurke <cyberzuchini@protonmail.com>
2024-08-26 16:12:07 +0200

    try to fix minimizing build

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-import TerserPlugin from 'terser-webpack-plugin';
 
 /** @type {import('next').NextConfig} */
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-    swcMinify: false, // Disable SWC minification
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.optimization.minimize = false; // Disable all minification
-        }
-        return config;
-    },
+
 };
 
 
