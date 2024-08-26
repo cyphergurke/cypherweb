05f7fbba8983a6cbd0ea3d84cc3d4a57f86a8d69
cyphergurke <cyberzuchini@protonmail.com>
2024-08-26 14:58:05 +0200

    another try to fix actions build

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
-
 import createNextIntlPlugin from 'next-intl/plugin';
+import TerserPlugin from 'terser-webpack-plugin';
 
 /** @type {import('next').NextConfig} */
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig = {};
+const nextConfig = {
+    swcMinify: false, // Disable SWC minification
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.optimization.minimize = false; // Disable all minification
+        }
+        return config;
+    },
+};
 
 
 export default withNextIntl(nextConfig);
